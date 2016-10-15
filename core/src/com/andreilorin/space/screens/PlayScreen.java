4cf0561cca82a8ea7f6a4894eb7137b38dc2d242
andrei lorin <andreilorin@gmail.com>
2016-10-15 21:59:29 +0100

    Created map loader and added map to assests

--- core/src/com/andreilorin/space/screens/PlayScreen.java
+++ core/src/com/andreilorin/space/screens/PlayScreen.java
@@ -3,10 +3,14 @@ package com.andreilorin.space.screens;
 import com.andreilorin.space.MyGame;
 import com.andreilorin.space.scenes.Hud;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -16,19 +20,39 @@ import com.badlogic.gdx.utils.viewport.Viewport;
  */
 public class PlayScreen implements Screen {
 
+    //Game field
     private MyGame game;
-    Texture texture;
+
+    //Camera fields
     private OrthographicCamera gameCamera;
     private Viewport gamePort;
     private Hud hud;
 
+    //Map fields
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+
 
     public PlayScreen(MyGame game){
         this.game = game;
-        texture = new Texture("BG.jpg");
+
+        //Create camera
         gameCamera = new OrthographicCamera();
+        //Create FitViewport
         gamePort = new FitViewport(MyGame.V_WIDTH, MyGame.V_HEIGHT, gameCamera);
+        //Create HUD
         hud = new Hud(game.batch);
+        //Create map loader
+        mapLoader = new TmxMapLoader();
+        //Load map
+        map = mapLoader.load("C:\\Users\\Lorin\\Desktop\\images\\spaceandrobots.tmx");//TODO cant load map, 32x32 tiles problem ?
+        //Render map
+        renderer = new OrthogonalTiledMapRenderer(map);
+        //Fit camera around viewport based on gameworld
+        gameCamera.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
     }
 
     @Override
@@ -36,10 +60,28 @@ public class PlayScreen implements Screen {
 
     }
 
+    //Handle input method
+    public void handleInput(float dt){
+        if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE))
+            gameCamera.position.x += 100 * dt;
+
+    }
+
+    //Gameworld update
+    public void update(float dt){
+        handleInput(dt);
+        gameCamera.update();
+        renderer.setView(gameCamera);
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
